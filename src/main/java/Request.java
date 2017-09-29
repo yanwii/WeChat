286f133efd006384ba47e8918c076c48450763ce
yanwii <yanwii@outlook.com>
2017-09-29 15:27:57 +0800

    添加二维码

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -2,6 +2,8 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Hashtable;
 
 public class Request {
@@ -12,19 +14,35 @@ public class Request {
     }
 
     public Hashtable<String,String> get(String url) {
-        Hashtable<String, String> response = new Hashtable<String, String>();
         System.setProperty ("jsse.enableSNIExtension", "false");
         try {
             Connection.Response con = Jsoup.connect(url).headers(this.headers).ignoreContentType(true).execute();
             int code = con.statusCode();
-            response.put("code", String.valueOf(code));
-            response.put("content", con.body());
+            return this.mkResponse(String.valueOf(code), con.body());
         } catch (Exception e){
-            response.put("code", "410");
-            response.put("content", e.toString());
+            return this.mkResponse("410", e.toString());
         }
-        return response;
     }
 
+    public Hashtable<String, String> saveImg(String url){
+        try{
+            Connection.Response con = Jsoup.connect(url).headers(this.headers).ignoreContentType(true).execute();
+            byte[] img = con.bodyAsBytes();
+            File imgFile = new File("./tmp/QR.jpg");
+            FileOutputStream outputStream = new FileOutputStream(imgFile);
+            outputStream.write(img);
+            outputStream.close();
+            return this.mkResponse("200", "succeed!");
+        } catch (Exception e){
+            return this.mkResponse("411", e.toString());
+        }
 
+    }
+
+    private Hashtable<String, String> mkResponse(String code, String content){
+        Hashtable<String, String> response = new Hashtable<String, String>();
+        response.put("code", code);
+        response.put("content", content);
+        return response;
+    }
 }
